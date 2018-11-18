47030cb9fa78854cffb472b4d97af9f096952dd8
Manas Gajare <manas.princeton@gmail.com>
2018-11-18 20:46:13 +0530

    Product Update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Model\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\Product\ProductResource;
 use App\Http\Resources\Product\ProductCollection;
-
+//use Symfony\Component\HttpFoundation\Response;
 class ProductController extends Controller
 {
+
+
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -35,9 +44,18 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $product = new Product;
+        $product->name = $request->name;
+        $product->detail = $request->description;
+        $product->stock = $request->stock;
+        $product->price = $request->price;
+        $product->discount = $request->discount;
+        $product->save();
+        return response([
+          'data' => new ProductResource($product)
+        ],200);
     }
 
     /**
@@ -72,7 +90,12 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $product->update($request->all());
+        //return $request->all();
+        //return $product;
+        return response([
+          'data' => new ProductResource($product)
+        ], 200);
     }
 
     /**
